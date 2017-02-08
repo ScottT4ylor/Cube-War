d4eeb9abab4de67cf42f5869897592f7793f949e
JasonRVCC <marcmanzx@yahoo.com>
2017-02-08 11:00:16 -0500

    Testing

--- Cube War (Working Title)/Assets/Cube.cs
+++ Cube War (Working Title)/Assets/Cube.cs
@@ -29,7 +29,8 @@ public class Cube : MonoBehaviour {
 	void Start () {
 		playState = PlayState.Idle; 
 		//cubePos = Camera.main.ScreenToWorldPoint (this.gameObject.GetComponent<Collider>().bounds.center);
-		cubePos = this.gameObject.GetComponent<Collider>().bounds.center;
+		//cubePos = this.gameObject.GetComponent<Collider>().bounds.center;
+		cubePos = this.gameObject.transform.position;
 		mousePos2D = Input.mousePosition;
 		mousePos3D = Vector3.zero;
 		//maxMagnitude = this.gameObject.GetComponent<Collider> ().bounds.size.x * 3f;
@@ -51,7 +52,7 @@ public class Cube : MonoBehaviour {
 		case PlayState.Launch:
 			print (velocity);
 			if (this.gameObject.GetComponent<Rigidbody> ().velocity == Vector3.zero) {
-				cubePos = this.gameObject.GetComponent<Collider>().bounds.center;
+				cubePos = this.gameObject.transform.position;
 				print (this.gameObject.GetComponent<Rigidbody> ().velocity);
 				this.gameObject.transform.rotation = Quaternion.Euler (0, 0, 0);
 				velocity = Vector3.zero;
@@ -75,10 +76,11 @@ public class Cube : MonoBehaviour {
 		//mousePos2D.z = cubePos.y;
 		mousePos2D.z = 5f;
 		mousePos3D = Camera.main.ScreenToWorldPoint (mousePos2D);
+		print (mousePos3D);
 		velocity = mousePos3D - cubePos;
 		//do something about the y later
 		velocity.y = 10f;
-		print (velocity);
+		//print (velocity);
 		//since the camera has been rotated, we need to change the vector values
 		/*float z = velocity.z;
 		velocity.z = velocity.y;
