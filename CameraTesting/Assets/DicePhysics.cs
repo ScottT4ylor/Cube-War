f86c54c12a6637dc00109f65b6e1d0ee3c5e7ec2
ScottT4ylor <G00236985@smail.raritanval.edu>
2017-02-14 18:11:33 -0500

    Started Physics Testing

---
+++ CameraTesting/Assets/DicePhysics.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DicePhysics : MonoBehaviour {
+    public Vector3 incomingForce;
+
+
+
+    private void FixedUpdate()
+    {
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        incomingForce = collision.impulse;
+        incomingForce.x *= Mathf.Min(1, -incomingForce.y);
+        incomingForce.z *= Mathf.Min(1, -incomingForce.y);
+        incomingForce.y /= 50;
+        GetComponent<Rigidbody>().AddForce(incomingForce);
+    }
+}
