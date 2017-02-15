8acaddfda2ccfd5255049f9e4c9ef61d97f05cb6
ScottT4ylor <G00236985@smail.raritanval.edu>
2017-02-15 11:46:53 -0500

    Started extra Dice physics
    Modifies dice physics interactions on hit based on which die is
    attacking, and increases knockback when hit by downward traveling die.
    Need to modify to make it only work when hit from above.

--- CameraTesting/Assets/DicePhysics.cs
+++ CameraTesting/Assets/DicePhysics.cs
@@ -4,20 +4,58 @@ using UnityEngine;
 
 public class DicePhysics : MonoBehaviour {
     public Vector3 incomingForce;
-
+    public bool activeDie;
+    public bool push = true;
 
 
     private void FixedUpdate()
     {
-
+        if(activeDie && push)
+        {
+            GetComponent<Rigidbody>().AddForce(10, 0, 0);
+        }
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        incomingForce = collision.impulse;
-        incomingForce.x *= Mathf.Min(1, -incomingForce.y);
-        incomingForce.z *= Mathf.Min(1, -incomingForce.y);
-        incomingForce.y /= 50;
-        GetComponent<Rigidbody>().AddForce(incomingForce);
+        if (!activeDie && collision.gameObject.CompareTag("Player"))
+        {
+
+
+            float incDown = collision.gameObject.GetComponent<Rigidbody>().velocity.y;
+            incomingForce = collision.impulse;
+            incomingForce.x += -collision.gameObject.GetComponent<Rigidbody>().velocity.x;
+            incomingForce.z += -collision.gameObject.GetComponent<Rigidbody>().velocity.z;
+            incomingForce *= Mathf.Pow(2, collision.gameObject.GetComponent<Rigidbody>().mass);
+            incomingForce.x *= Mathf.Max(1, -incDown);
+            incomingForce.z *= Mathf.Max(1, -incDown);
+            incomingForce.y /= 50;
+            GetComponent<Rigidbody>().AddForce(-incomingForce * GetComponent<Rigidbody>().mass * 50);
+
+
+            //print(name + " - Hit, adding force of: " + incomingForce * GetComponent<Rigidbody>().mass + " from: " + GetComponent<Rigidbody>().mass);
+        }
+        else if (activeDie && collision.gameObject.CompareTag("Player"))
+        {
+            push = false;
+
+
+            float incDown = collision.gameObject.GetComponent<Rigidbody>().velocity.y;
+            incomingForce = collision.impulse;
+            incomingForce.x += -GetComponent<Rigidbody>().velocity.x;
+            incomingForce.z += -GetComponent<Rigidbody>().velocity.z;
+            incomingForce.x *= Mathf.Max(1, -incDown);
+            incomingForce.z *= Mathf.Max(1, -incDown);
+            incomingForce.y /= 50;
+            GetComponent<Rigidbody>().AddForce(incomingForce * GetComponent<Rigidbody>().mass *50);
+
+            print(GetComponent<Rigidbody>().mass);
+            print(name + " - Hit, adding force of: " + incomingForce * Mathf.Pow(2, GetComponent<Rigidbody>().mass) * 100);
+        }
+    }
+
+        public bool retPush()
+    {
+        return push;
     }
 }
