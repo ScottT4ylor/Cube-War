42ef28ce82bd134fa312c7ba80a5da8a40a35ac3
ScottT4ylor <G00236985@smail.raritanval.edu>
2017-02-22 12:20:05 -0500

    Set up Camera Interface Script
    Also modified the movement script to work with the camera interface.

---
+++ CameraTesting/Assets/CameraInterface.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+/// <summary>
+/// 
+/// Camera interface for controlling the camera.
+/// 
+/// Contains functions for moving the camera's focus to specific points or objects.
+/// 
+/// </summary>
+
+public static class CameraInterface {
+
+    public static GameObject cameraAnchor;
+
+    public static void Focus(Vector3 dest)
+    {
+        cameraAnchor.GetComponent<Movement>().setFocusDestination(dest);
+    }
+
+    public static void Focus(GameObject obj)
+    {
+        cameraAnchor.GetComponent<Movement>().setFocusDestination(obj.transform.position);
+    }
+
+    public static Vector3 getCameraFocus()
+    {
+        return cameraAnchor.transform.position;
+    }
+}
