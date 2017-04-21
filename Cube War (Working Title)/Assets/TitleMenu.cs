ee58067e659f99869a5f1a5f9bc08ffd89739c64
JasonRVCC <marcmanzx@yahoo.com>
2017-04-21 16:02:27 -0400

    Game Over reset & menu

--- Cube War (Working Title)/Assets/TitleMenu.cs
+++ Cube War (Working Title)/Assets/TitleMenu.cs
@@ -12,6 +12,6 @@ public class TitleMenu : MonoBehaviour {
 	}
 	
 	public void StartGame(){
-		SceneManager.LoadScene ("InterfaceSetup", LoadSceneMode.Single);
+		SceneManager.LoadScene ("Playable", LoadSceneMode.Single);
 	}
 }
